04271d7a344354152abbbe1c9be411a5c3eb7d84
ThiagoMarques <thiagossmarques@gmail.com>
2022-06-16 21:25:32 -0300

    adiciona novo componente offer-item

--- android/app/src/main/java/tesp/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package tesp.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { Routes, RouterModule } from '@angular/router'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { OffersPage } from './offers.page';
+import { OffersPage } from './offers.page'
+import { OfferItemComponent } from './offer-item/offer-item.component'
 
 const routes: Routes = [
   {
     path: '',
-    component: OffersPage
-  }
-];
+    component: OffersPage,
+  },
+]
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
-  declarations: [OffersPage]
+  declarations: [OffersPage, OfferItemComponent],
 })
 export class OffersPageModule {}
