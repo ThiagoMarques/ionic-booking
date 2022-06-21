d17f06917b4f04dcccc5fe8d84226c47f25083e5
ThiagoMarques <thiagossmarques@gmail.com>
2022-06-20 21:25:00 -0300

    Adiciona formulário para edição de locais

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,31 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { NavController } from '@ionic/angular'
 
-import { PlacesService } from '../../places.service';
-import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service'
+import { Place } from '../../place.model'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss']
+  styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
-  place: Place;
+  place: Place
+  form: FormGroup
 
   constructor(
     private route: ActivatedRoute,
     private placesService: PlacesService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
   ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
+    this.route.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('placeId')) {
-        this.navCtrl.navigateBack('/places/tabs/offers');
-        return;
+        this.navCtrl.navigateBack('/places/tabs/offers')
+        return
       }
-      this.place = this.placesService.getPlace(paramMap.get('placeId'));
-    });
+      this.place = this.placesService.getPlace(paramMap.get('placeId'))
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required],
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)],
+        }),
+      })
+    })
+  }
+
+  onUpdateOffer() {
+    if (!this.form.valid) {
+      return
+    }
+    console.log(this.form)
   }
 }
