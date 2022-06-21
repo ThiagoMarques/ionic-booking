9222ddae84bd6e175e544be511a6f6b4c3115842
ThiagoMarques <thiagossmarques@gmail.com>
2022-06-20 21:04:31 -0300

    Inserção de validadores em componentes reativos

--- src/app/places/offers/new-offer/new-offer.module.ts
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+import { Routes, RouterModule } from '@angular/router'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { NewOfferPage } from './new-offer.page';
+import { NewOfferPage } from './new-offer.page'
 
 const routes: Routes = [
   {
     path: '',
-    component: NewOfferPage
-  }
-];
+    component: NewOfferPage,
+  },
+]
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
-  declarations: [NewOfferPage]
+  declarations: [NewOfferPage],
 })
 export class NewOfferPageModule {}
